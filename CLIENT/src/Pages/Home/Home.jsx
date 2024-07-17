dc6c5df24028171bc8844f4daf6862cd88f36870
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-07-17 09:13:43 +0300

    complete hero section

--- CLIENT/src/Pages/Home/Home.jsx
+++ CLIENT/src/Pages/Home/Home.jsx
@@ -3,27 +3,37 @@ import TypingAnimator from 'react-typing-animator';
 import "./Home.css"
 
 function Home() {
-  const textArray = ['near you', 'Call Us ','On','+254768163608'];
+  const textArray = ['near you', 'Call Us on','+254768163608'];
+  const animation = <TypingAnimator
+        
+  textArray= {textArray}
+  cursorColor="red"
+  textColor="red"
+  fontSize="2rem"
+  loop
+  typingSpeed={60}
+  delaySpeed={1000}
+  backspace
+  height="60px"
+/>
   return (
     <>
     <div className='herocontainer'>
       <div className="heroheading">
-        <h1>Order delivery 
-        <TypingAnimator
-        
-      textArray= {textArray}
-      cursorColor="#333"
-      textColor="#000000"
-      fontSize="2rem"
-      loop
-      typingSpeed={60}
-      delaySpeed={1000}
-      backspace
-      height="60px"
-    />
-    </h1>
+        <h1>Order delivery:{animation} </h1>
       </div>
+
+      <div className="heroinputs">
+      <input type="text"  placeholder='Enter delivery address '/>
+      <select>
+        <option value="Deliver Now">Deliver Now</option>
+        <option value="Deliver Now">Order Delivery</option>
+        <option value="Deliver Now">Cancel Delivery</option>
+      </select>
+      <button>Find Food</button>
     </div>
+    </div>
+
     </>
   )
 }

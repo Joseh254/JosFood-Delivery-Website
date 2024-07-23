ad214ba00469199ea320accfc5f169a083d2ef07
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-07-23 10:43:23 +0300

    verify cart

--- SERVER/Routes/cartRoute.js
+++ SERVER/Routes/cartRoute.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { createCart, removeCart,getCart } from "../Controllers/cartContoller.js";
+import VerifyCart from "../Middleware/verifyCartUser";
 const router = Router();
 router.post("/createCart", createCart);
-router.get("/getCart/:id",getCart)
-router.delete("/removeCart/:id", removeCart);
+router.get("/getCart/:id", VerifyCart, getCart)
+router.delete("/removeCart/:id", VerifyCart, removeCart);
 
 export default router;
